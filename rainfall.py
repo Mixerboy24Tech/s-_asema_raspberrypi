ca967eb5373e8e5eec674b6797435cf7506cf259
Atte Oksanen <37768995+Mixerboy24@users.noreply.github.com>
2020-07-20 14:47:05 +0300

    Create rainfall.py

---
+++ rainfall.py
@@ -0,0 +1,17 @@
+rom gpiozero import Button
+
+rain_sensor = Button(6)
+BUCKET_SIZE = 0.2
+count = 0
+
+def bucket_tipped():
+        global count
+        count = count + 1
+        print (count * BUCKET_SIZE)
+
+def reset_rainfall():
+        global count
+        count = 0
+
+while True:
+        rain_sensor.when_pressed = bucket_tipped
